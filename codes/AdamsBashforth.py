97d674527974476ff3591f6f33a7747965dad36c
suhasdl <suhasdl.mysore@gmail.com>
2019-02-27 19:03:09 +0530

    Add some files

---
+++ codes/AdamsBashforth.py
@@ -0,0 +1,36 @@
+"""
+Code for time stepping using a 3rd order Adams-Bashforth method.
+First two steps need to be initialised through other meanns.
+Here euler's forward method and 2nd order Adams-Bashforth is uded for first two timesteps.
+"""
+
+import numpy as np
+
+class AdamBash(object):
+    # you can input the hyper-viscosity diffusion function
+    def __init__(self, dfdt, diffusion = None, ncycle = 0):
+        self.ncycle = ncycle
+        self.dfdt = dfdt
+        self.diffusion = diffusion
+
+    # Update fields with 3rd order adams-bashforth method
+    def integrate(self, t, fields,dt, args = None):
+        F = np.array(fields)
+        F1 = F.copy()
+        fnew = np.array(self.dfdt(t,F))
+        #forward euler, then 2nd order adams-bashforth time stepping
+        if self.ncycle==0:
+            self.fnow = fnew.copy()
+            f1 = F1 + dt*fnew   # Euler
+        elif self.ncycle==1:
+            f1 = F1 + dt*((3/2.)*fnew - (1/2.)*self.fnow)  #2nd order AB
+        else:
+            f1 = F1 + dt*((23/12.)*fnew - (16/12.)*self.fnow + (5/12.)*self.fold) #3rd order AB
+
+        if diffusion != None:
+            fq = self.diffusion(dt,f1)
+        tnew = t+dt
+        self.fold = self.fnow.copy()
+        self.fnow = fnew.copy()
+        self.ncycle += 1
+        return (tnew,fq)
---
+++ codes/sphTrans.py
@@ -0,0 +1,59 @@
+"""
+Wrapper class for commonly used spectral transform operations in
+atmospheric models.  Provides an interface to shtns compatible
+with pyspharm (pyspharm.googlecode.com).
+Copied from shtns example file with slight modifications.
+"""
+
+import numpy as np
+import shtns
+
+class Spharmt(object):
+    def __init__(self,nlons,nlats,ntrunc,rsphere,gridtype='gaussian'):
+        """initialize
+        nlons:  number of longitudes
+        nlats:  number of latitudes"""
+        self._shtns = shtns.sht(ntrunc, ntrunc, 1, \
+                shtns.sht_orthonormal+shtns.SHT_NO_CS_PHASE)
+        if gridtype == 'gaussian':
+            #self._shtns.set_grid(nlats,nlons,shtns.sht_gauss_fly|shtns.SHT_PHI_CONTIGUOUS,1.e-10)
+            self._shtns.set_grid(nlats,nlons,shtns.sht_quick_init|shtns.SHT_PHI_CONTIGUOUS,1.e-10)
+        elif gridtype == 'regular':
+            self._shtns.set_grid(nlats,nlons,shtns.sht_reg_dct|shtns.SHT_PHI_CONTIGUOUS,1.e-10)
+        self.lats = np.arcsin(self._shtns.cos_theta)
+        self.lons = (2.*np.pi/nlons)*np.arange(nlons)
+        self.nlons = nlons
+        self.nlats = nlats
+        self.ntrunc = ntrunc
+        self.nlm = self._shtns.nlm
+        self.degree = self._shtns.l
+        self.lap = -self.degree*(self.degree+1.0).astype(np.complex)
+        self.invlap = np.zeros(self.lap.shape, self.lap.dtype)
+        self.invlap[1:] = 1./self.lap[1:]
+        self.rsphere = rsphere
+        self.lap = self.lap/self.rsphere**2
+        self.invlap = self.invlap*self.rsphere**2
+
+    def grdtospec(self,data):
+        """compute spectral coefficients from gridded data"""
+        return self._shtns.analys(data)
+
+    def spectogrd(self,dataspec):
+        """compute gridded data from spectral coefficients"""
+        return self._shtns.synth(dataspec)
+
+    def getuv(self,vrtspec,divspec):
+        """compute wind vector from spectral coeffs of vorticity and divergence"""
+        return self._shtns.synth((self.invlap/self.rsphere)*vrtspec,
+                (self.invlap/self.rsphere)*divspec)
+
+    def getvrtdivspec(self,u,v):
+        """compute spectral coeffs of vorticity and divergence from wind vector"""
+        vrtspec, divspec = self._shtns.analys(u, v)
+        return self.lap*self.rsphere*vrtspec, self.lap*self.rsphere*divspec
+
+    def getgrad(self,divspec):
+        """compute gradient vector from spectral coeffs"""
+        vrtspec = np.zeros(divspec.shape, dtype=np.complex)
+        u,v = self._shtns.synth(vrtspec,divspec)
+        return u/self.rsphere, v/self.rsphere
